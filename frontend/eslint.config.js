638993c1ad50687d03b765e3fab60f7cf5baaac9
dudu_win10 <dudu@gmail.com>
2025-04-05 22:30:23 +1300

    added eslint-plugin-import

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -3,12 +3,13 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import react from 'eslint-plugin-react'
+import importPlugin from 'eslint-plugin-import';
 
 export default [
   { ignores: ['dist'] },
   {
     files: ['**/*.{js,jsx}'],
-    languageOptions: {
+    languageOptions: {  
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
@@ -21,6 +22,7 @@ export default [
       'react': react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'import': importPlugin,
     },
     settings: {
       react: {
@@ -32,11 +34,13 @@ export default [
       ...reactHooks.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
+      ...importPlugin.configs.recommended.rules,
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
+      'import/namespace': 'off',
     },
   },
 ]
