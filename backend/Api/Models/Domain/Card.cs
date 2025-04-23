d4115aec1ead00a55a49000ac4520cf05cf5415e
Henry Chien <henry.cychien@gmail.com>
2025-04-24 11:33:30 +1200

    fix(domain): add missing bracket for namespace

--- backend/Api/Models/Domain/Card.cs
+++ backend/Api/Models/Domain/Card.cs
@@ -1,13 +1,15 @@
 using System;
 
 namespace Api.Models.Domain
-public class Card
 {
-    public int Id { get; set; }
-    public required string Name { get; set; }  // Card name
-    public required string Number { get; set; }  // if major arcana: 0~21, else if minor arcana: 1~10, Page, Knight, Queen, King
-    public required string ImageSource { get; set; }  // PNG
-    public required string Description { get; set; }  // Card description text
-    public required bool IsMajorArcana { get; set; }  // Indicates if the card is a major arcana or a minor arcana
-    public string? Suit { get; set; }  // if minor arcana: Cups, Wands, Swords, Pentacles
+    public class Card
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; } // Card name
+        public required string Number { get; set; } // if major arcana: 0~21, else if minor arcana: 1~10, Page, Knight, Queen, King
+        public required string ImageSource { get; set; } // PNG
+        public required string Description { get; set; } // Card description text
+        public required bool IsMajorArcana { get; set; } // Indicates if the card is a major arcana or a minor arcana
+        public string? Suit { get; set; } // if minor arcana: Cups, Wands, Swords, Pentacles
+    }
 }
