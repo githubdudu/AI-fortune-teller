f1e7b55b9d71089c5c487952fa5d4f74b33c9cb5
dudu_win10 <35301543+githubdudu@users.noreply.github.com>
2025-04-23 14:21:56 +1200

    added root layout for all the page

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -14,8 +14,6 @@ function HomePage() {
 
   return (
     <>
-      <ArcanaVerseLogo />
-      <h1 className="text-5xl leading-tight">ArcanaVerse</h1>
       <div className="p-8">
         <button
           className="px-5 py-2 rounded-lg border border-w-1 border-solid border-transparent bg-slate-100 font-medium hover:border-indigo-500 transition-colors duration-[0.25s] cursor-pointer focus:ring-4 focus:ring-indigo-500 focus-visible:ring-4 focus-visible:ring-indigo-500"
---
+++ frontend/src/pages/RootLayoutPage/RootLayoutPage.jsx
@@ -0,0 +1,27 @@
+import { Outlet } from 'react-router-dom';
+import { Box, Flex } from 'gestalt';
+
+import ArcanaVerseLogo from '../../components/ArcanaVerseLogo';
+
+function RootLayoutPage() {
+  return (
+    <Flex
+      alignItems="center"
+      justifyContent="start"
+      direction="column"
+      height="100vh"
+    >
+      <Box marginBottom={12}>
+        <ArcanaVerseLogo />
+      </Box>
+      <Flex alignItems="center" justifyContent="start" direction="column">
+        <Outlet />
+        <Outlet />
+        <Outlet />
+        <Outlet />
+      </Flex>
+    </Flex>
+  );
+}
+
+export default RootLayoutPage;
