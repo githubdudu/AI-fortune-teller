f4785b997f38701f0ea7bbc100c9c8b35f4afe0b
dudu_win10 <35301543+githubdudu@users.noreply.github.com>
2025-04-25 20:24:54 +1200

    added validation

--- frontend/src/pages/UserInfoInputPage/NationalityInputBox.jsx
+++ frontend/src/pages/UserInfoInputPage/NationalityInputBox.jsx
@@ -1,38 +1,61 @@
 import { useState } from 'react';
 import { ComboBox } from 'gestalt';
+import PropTypes from 'prop-types';
 
 import { COUNTRY_OPTIONS } from '../../constants/nationality';
-function NationalityInputBox() {
-  const [errorMessage, setErrorMessage] = useState();
 
-  const handleBlur = (event) => {
-    const value = event?.value;
-    if (
-      value !== '' &&
-      !COUNTRY_OPTIONS.some((option) => option.value === value)
-    ) {
-      setErrorMessage('Invalid country selected');
+function NationalityInputBox({ inputValue, setInputValue }) {
+  const [selected, setSelected] = useState();
+  const [suggestedOptions, setSuggestedOptions] = useState(COUNTRY_OPTIONS);
+
+  const handleOnChange = ({ value }) => {
+    setSelected();
+    if (value) {
+      setInputValue(value);
+      const filteredOptions = COUNTRY_OPTIONS.filter((item) =>
+        item.label.toLowerCase().includes(value.toLowerCase()),
+      );
+      setSuggestedOptions(filteredOptions);
+    } else {
+      setInputValue(value);
+      setSuggestedOptions(COUNTRY_OPTIONS);
     }
   };
 
-  const resetErrorMessage = () => {
-    if (errorMessage) {
-      setErrorMessage();
-    }
+  const handleSelect = ({ item }) => {
+    setInputValue(item.label);
+    setSuggestedOptions(COUNTRY_OPTIONS);
+    setSelected(item);
   };
 
   return (
     <ComboBox
+      accessibilityClearButtonLabel="Clear the current value"
       id="nationality"
+      inputValue={inputValue}
       noResultText="No results"
-      onBlur={handleBlur}
-      onChange={resetErrorMessage}
-      onClear={resetErrorMessage}
-      options={COUNTRY_OPTIONS}
+      onBlur={() => {
+        if (!selected) setInputValue('');
+        setSuggestedOptions(COUNTRY_OPTIONS);
+      }}
+      onChange={handleOnChange}
+      onClear={() => {
+        setInputValue('');
+        setSelected();
+        setSuggestedOptions(COUNTRY_OPTIONS);
+      }}
+      onSelect={handleSelect}
+      options={suggestedOptions}
       placeholder="Nationality"
+      selectedOption={selected}
       size="lg"
     />
   );
 }
+
+NationalityInputBox.propTypes = {
+  inputValue: PropTypes.string.isRequired,
+  setInputValue: PropTypes.func.isRequired,
+};
 
 export default NationalityInputBox;
