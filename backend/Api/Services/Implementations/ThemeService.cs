a5688e5f7a63514155e93c3fa7121e1829891aa1
jinwookuk <jkuk801@aucklanduni.ac.nz>
2025-04-26 12:49:14 +1200

    Add ThemeService.cs, Fix IThemeService.cs GetThemeByIdAsync()'s parameter

---
+++ backend/Api/Services/Implementations/ThemeService.cs
@@ -0,0 +1,40 @@
+using Api.Models.Domain;
+using Api.Models.DTOs;
+using Api.Models.Requests;
+using Api.Repositories.Interfaces;
+using Api.Services.Interfaces;
+
+namespace Api.Services.Implementations
+{
+    public class ThemeService : IThemeService
+    {
+        private readonly IThemeRepository _themeRepository;
+        public ThemeService(IThemeRepository themeRepository)
+        {
+            _themeRepository = themeRepository;
+        }
+
+        public async Task<IEnumerable<ThemeDto>> GetAllThemesAsync()
+        {
+            var themes = await _themeRepository.GetAllAsync();
+            return themes.Select(MapThemeToDto);
+        }
+
+        public async Task<ThemeDto?> GetThemeByIdAsync(int id)
+        {
+            var theme = await _themeRepository.GetByIdAsync(id);
+            return theme != null ? MapThemeToDto(theme) : null;
+        }
+
+        private static ThemeDto MapThemeToDto(Theme theme)
+        {
+            return new ThemeDto
+            {
+                Id = theme.Id,
+                Name = theme.Name,
+                ImageSource = theme.ImageSource,
+                Description = theme.Description
+            };
+        }
+    }
+}
