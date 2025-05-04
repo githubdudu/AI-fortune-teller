7485e4b5ee1c730337d30fc1e7d41a6126c2e3bd
dudu_win10 <35301543+githubdudu@users.noreply.github.com>
2025-05-04 20:05:26 +1200

    add context for UserQuestionInput.jsx

--- frontend/src/context/AppContextProvider/AppContextProvider.jsx
+++ frontend/src/context/AppContextProvider/AppContextProvider.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { AppContext } from './AppContext.jsx';
+import { useSessionStorage } from 'react-use';
 
 export function AppContextProvider({ children }) {
   const [count, setCount] = useState(0);
 
+  const [userPrompt, setUserPrompt] = useSessionStorage('userPrompt', '');
+
   return (
-    <AppContext.Provider value={{ count, setCount }}>
+    <AppContext.Provider value={{ count, setCount, userPrompt, setUserPrompt }}>
       {children}
     </AppContext.Provider>
   );
