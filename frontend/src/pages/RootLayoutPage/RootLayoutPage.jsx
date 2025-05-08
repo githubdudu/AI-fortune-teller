624b7d4d46dcf7bd2c25b6e951952a925a0a4fc4
dudu_win10 <35301543+githubdudu@users.noreply.github.com>
2025-05-08 19:37:40 +1200

    added tab bar routing function

--- frontend/src/pages/RootLayoutPage/RootLayoutPage.jsx
+++ frontend/src/pages/RootLayoutPage/RootLayoutPage.jsx
@@ -1,9 +1,31 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet , useNavigate } from 'react-router-dom';
 import { Box, Flex, Sticky, Tabs } from 'gestalt';
+import { useState } from 'react';
 
 import ArcanaVerseLogo from '../../components/ArcanaVerseLogo';
 
 function RootLayoutPage() {
+  const navigator = useNavigate();
+  const [selectedTabIndex, setSelectedTabIndex] = useState(0);
+  const handleTabChange = ({ event, activeTabIndex, ...rest }) => {
+    event.preventDefault();
+    setSelectedTabIndex(activeTabIndex);
+    console.log({ event }, { rest });
+    const buttonName = event.target.innerText;
+
+    // Navigate to home
+    if (buttonName === 'Home') {
+      navigator('/');
+    } else if (buttonName === 'About') {
+      navigator('/about');
+    } else if (buttonName === 'Login') {
+      navigator('/login');
+    }
+  };
+
+  const handleTabChangeForRightTabs = ({ activeTabIndex, ...rest }) => {
+    handleTabChange({ ...rest, activeTabIndex: activeTabIndex + 1 });
+  };
   return (
     <Flex
       alignItems="center"
@@ -14,19 +36,19 @@ function RootLayoutPage() {
       <Sticky top={0}>
         <div className="bg-white w-screen flex justify-between px-4 shadow-md">
           <Tabs
-            tabs={[{ text: 'Home', href: '/' }]}
-            onChange={() => {}}
-            selectedTabIndex={0}
+            tabs={[{ text: 'Home', href: '/', id: 'home' }]}
+            onChange={handleTabChange}
+            activeTabIndex={selectedTabIndex}
             size="md"
             accessibilityLabel="Main navigation"
           />
           <Tabs
             tabs={[
-              { text: 'About', href: '/about' },
-              { text: 'Login', href: '/login' },
+              { text: 'About', href: '/about', id: 'about' },
+              { text: 'Login', href: '/login', id: 'login' },
             ]}
-            onChange={() => {}}
-            selectedTabIndex={1}
+            onChange={handleTabChangeForRightTabs}
+            activeTabIndex={selectedTabIndex - 1}
             size="md"
             accessibilityLabel="Main navigation"
           />
