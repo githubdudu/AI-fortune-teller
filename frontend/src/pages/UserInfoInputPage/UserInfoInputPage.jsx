71c5f4af4734418b7bc5c95563aae31827d246ae
dudu_win10 <35301543+githubdudu@users.noreply.github.com>
2025-05-08 22:40:21 +1200

    extract Form header

--- frontend/src/pages/UserInfoInputPage/UserInfoInputPage.jsx
+++ frontend/src/pages/UserInfoInputPage/UserInfoInputPage.jsx
@@ -1,10 +1,12 @@
 import UserDetailsForm from '$/components/UserDetailsForm';
-import UserInfoHeader from './UserInfoHeader';
+import FormTitle from '$/components/FormTitle';
 
 function UserInfoInputPage() {
+  const HEADER_TITLE = 'Enter your details';
+  const HEADER_SUBTITLE = 'These help us tailor your cosmic reading experience';
   return (
     <div className="px-9 py-5 flex flex-col items-center bg-gray-50 rounded-xl shadow-md">
-      <UserInfoHeader />
+      <FormTitle title={HEADER_TITLE} subtitle={HEADER_SUBTITLE} />
       <UserDetailsForm />
     </div>
   );
