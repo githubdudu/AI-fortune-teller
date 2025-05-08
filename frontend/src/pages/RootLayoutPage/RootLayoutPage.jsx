1233d5fcab9c14baa8d5928b36cd39d83dba9f50
dudu_win10 <35301543+githubdudu@users.noreply.github.com>
2025-05-08 19:02:32 +1200

    added nav bar

--- frontend/src/pages/RootLayoutPage/RootLayoutPage.jsx
+++ frontend/src/pages/RootLayoutPage/RootLayoutPage.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom';
-import { Box, Flex } from 'gestalt';
+import { Box, Flex, Sticky, Tabs } from 'gestalt';
 
 import ArcanaVerseLogo from '../../components/ArcanaVerseLogo';
 
@@ -11,6 +11,28 @@ function RootLayoutPage() {
       direction="column"
       height="100vh"
     >
+      <Sticky top={0}>
+        <div className="bg-white w-screen flex justify-between px-4 shadow-md">
+          <Tabs
+            tabs={[{ text: 'Home', href: '/' }]}
+            onChange={() => {}}
+            selectedTabIndex={0}
+            size="md"
+            accessibilityLabel="Main navigation"
+          />
+          <Tabs
+            tabs={[
+              { text: 'About', href: '/about' },
+              { text: 'Login', href: '/login' },
+            ]}
+            onChange={() => {}}
+            selectedTabIndex={1}
+            size="md"
+            accessibilityLabel="Main navigation"
+          />
+        </div>
+      </Sticky>
+
       <Box marginBottom={10}>
         <ArcanaVerseLogo />
       </Box>
