15552cb7ac30d6ad27cc88fa5771e43aa3940ae2
jinwookuk <jkuk801@aucklanduni.ac.nz>
2025-05-13 09:28:56 +1200

    chore: update package.json and vite.config.js for testing setup
    - Added "vitest" script to package.json for running tests.
    - Included "vitest" as a dependency in package.json.
    - Added "axios-mock-adapter" and "jsdom" as devDependencies.
    - Updated vite.config.js to configure test environment with jsdom and enable global variables.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 // eslint-disable-next-line import/no-unresolved
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       $: '/src',
     },
   },
-})
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
+});
