a7e2fa0d1b7628d6b9a108b7007649de64583e8b
dudu_win10 <35301543+githubdudu@users.noreply.github.com>
2025-05-13 16:19:20 +1200

    update card styles, added color palattes from Figma design. fixed number css

--- frontend/src/pages/FortunePage/FortunePage.jsx
+++ frontend/src/pages/FortunePage/FortunePage.jsx
@@ -346,7 +346,11 @@ const FortunePage = () => {
       <div className="card-container">
         {cards.map((card) => (
           // Check if the card is already selected
-          <div key={card.id} onClick={() => handleCardSelect(card.id)}>
+          <div
+            key={card.id}
+            className="card-wrapper"
+            onClick={() => handleCardSelect(card.id)}
+          >
             <Card
               backImage="https://tarot-card-images-all.s3.ap-southeast-2.amazonaws.com/TarotCardBackCard.png"
               frontImage={card.imageSource || '/defaultFrontCard.png'}
