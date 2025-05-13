881ac71523d795f6099996fd3389974fb95237be
dudu_win10 <35301543+githubdudu@users.noreply.github.com>
2025-05-13 21:37:04 +1200

    added error message display on user info input page

--- frontend/src/components/UserDetailsForm/UserDetailsForm.jsx
+++ frontend/src/components/UserDetailsForm/UserDetailsForm.jsx
@@ -2,6 +2,7 @@ import { Button, Fieldset, TextField, SelectList } from 'gestalt';
 import { DatePicker } from 'gestalt-datepicker';
 import { useState, useContext, useEffect } from 'react';
 import axios from 'axios';
+import PropTypes from 'prop-types';
 
 import NationalityInputBox from '$/components/NationalityInputBox';
 import { useNavigate } from 'react-router-dom';
@@ -33,6 +34,7 @@ function UserDetailsForm() {
   const [genderValue, setGenderValue] = useState(null);
   const [nationalityValue, setNationalityValue] = useState(null);
   const [POBValue, setPOBValue] = useState(null);
+  const [requestErrorMessage, setRequestErrorMessage] = useState('');
 
   const navigate = useNavigate();
 
@@ -97,6 +99,9 @@ function UserDetailsForm() {
       navigate('/');
     } catch (error) {
       console.error('Error putting user request:', error);
+      setRequestErrorMessage(
+        'An error occurred while updating your profile. Please try again later.',
+      );
       return;
     }
   }
@@ -146,10 +151,12 @@ function UserDetailsForm() {
     setGenderValue(value);
   }
 
+  ErrorMessage.propTypes = {
+    message: PropTypes.string,
+  };
+
   return (
-    <form
-      onSubmit={handleSubmit}
-    >
+    <form onSubmit={handleSubmit}>
       <Fieldset legend="type your details" legendDisplay="hidden">
         <div className="flex flex-col gap-4">
           <div className="flex flex-col md:flex-row gap-4">
@@ -166,6 +173,7 @@ function UserDetailsForm() {
               <Residency />
             </div>
           </div>
+          <ErrorMessage message={requestErrorMessage} />
           {ProceedButton()}
         </div>
       </Fieldset>
@@ -253,9 +261,25 @@ function UserDetailsForm() {
     );
   }
 
+  function ErrorMessage({ message }) {
+    return (
+      <div className="flex flex-col gap-4">
+        {message && (
+          <div className="text-red-500 text-sm text-center">{message}</div>
+        )}
+      </div>
+    );
+  }
+
   function ProceedButton() {
     return (
-      <Button text="Save and Continue" type="submit" name="edit-button" size="lg" color="red" />
+      <Button
+        text="Save and Continue"
+        type="submit"
+        name="edit-button"
+        size="lg"
+        color="red"
+      />
     );
   }
 }
